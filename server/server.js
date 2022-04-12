3571d764ccca0f1a89539049ca4cdd08ce8f89cd
minshaf998 <minshaf998@gmail.com>
2022-04-12 23:54:45 +0530

    logger middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const Joi = require('joi');
 const express = require('express');
 const admins = require('./routes/admins')
+const logger = require('./middleware/logger');
 const app = express();
 
 app.use(express.json());
+// app.use(logger);
 app.use('/api/admins', admins);
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}... by isf`));
