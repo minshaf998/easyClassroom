cdf02e678fff6b35eace05d6545e1f400ce10239
minshaf998 <minshaf998@gmail.com>
2022-06-06 13:25:10 +0530

    all right

--- app/Authendication/LoadingScreen.js
+++ app/Authendication/LoadingScreen.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import { ActivityIndicator, StyleSheet, View ,Text,Image} from 'react-native';
+import { ActivityIndicator, StyleSheet, View, Text, Image } from 'react-native';
 import * as firebase from 'firebase';
 import "firebase/firestore";
 
-export default function LoadingScreen({ navigation }){
-
-  
-
-    firebase.auth().onAuthStateChanged(function(user){
-          if (user) {
-             navigation.navigate('Dashboard');
-
-          }else {
-              navigation.navigate('Home'); 
-            }
-          });
+export default function LoadingScreen({ navigation }) {
 
 
-    
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      navigation.navigate('Dashboard');
+
+    } else {
+      navigation.navigate('Home');
+    }
+  });
+
+
+
 
 
   return (
     <View style={styles.container}>
-     
-        <Image
-          style={styles.logo}
-          source={require("../assets/logo.png")}
-        ></Image>
-        <Text style={{color: 'black', fontSize: 12,marginBottom:20, marginStart:-10}}>Easy Classroom</Text>
-        <ActivityIndicator color="blue" size="large" />
-     
+      <Image
+        style={styles.logo}
+        source={require("../assets/logo.png")}
+      ></Image>
+      <Text style={{ color: 'black', fontSize: 40 }}>Easy Classroom</Text>
+      <ActivityIndicator color="blue" size="large" />
     </View>
   );
 
@@ -38,18 +36,13 @@ export default function LoadingScreen({ navigation }){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:'#ffffff'
+    backgroundColor: '#ffffff'
   },
 
   logo: {
-    
-    width: 150,
-    height: 150,
-    marginBottom:-40,
-    marginTop:-60
+    width: 200,
+    height: 200,
   },
-  
 });
